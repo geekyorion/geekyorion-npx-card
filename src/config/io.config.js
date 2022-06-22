442d940adf06c0cb7624208b8ddd87c0d8d524d4
Shashank Sharma <shashank.ns67@gmail.com>
2022-06-22 11:43:54 +0530

    io path config

--- src/config/io.config.js
+++ src/config/io.config.js
@@ -1,1 +1,5 @@
-export const filePath = 'bin/output';
+import path from 'path';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export const filePath = path.join(__dirname, '../../bin/output');
